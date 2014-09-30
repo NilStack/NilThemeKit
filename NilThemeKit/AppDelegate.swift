1c7c6aa40bda1a282143fc3d42c1470ff2461bfb
Peng Guo <guoleii@gmail.com>
2014-09-30 16:01:17 +0800

    first commit

--- NilThemeKit/AppDelegate.swift
+++ NilThemeKit/AppDelegate.swift
@@ -16,29 +16,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.whiteColor()
+        
+        NilThemeKit.setupTheme(primaryColor: NilThemeKit.color(r: 0.0, g: 184.0, b: 156.0), secondaryColor: UIColor.whiteColor(), fontname: "HelveticaNeue-Light", lightStatusBar:true)
+        let mainVC: ViewController = ViewController()
+        let navigationController: UINavigationController = UINavigationController(rootViewController: mainVC)
+        mainVC.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarSystemItem.Favorites, tag: 0)
+        navigationController.navigationBar.translucent = false
+        
+        let tabBarController: UITabBarController = UITabBarController()
+        tabBarController.viewControllers = [navigationController]
+        tabBarController.tabBar.translucent = false
+        window?.rootViewController = tabBarController
+        
+        window?.makeKeyAndVisible()
         return true
-    }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
--- NilThemeKit/ViewController.swift
+++ NilThemeKit/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate
+{
+    let searchBar: UISearchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        title = "Main View"
+        
+        let toolBar: UIToolbar = UIToolbar(frame: CGRectMake(0.0, 0.0, CGRectGetWidth(view.frame), 35.0))
+        let flexibleItem: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        let dismissButton: UIBarButtonItem = UIBarButtonItem(title: "Dismiss", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("dismiss") )
+        toolBar.items = [flexibleItem, dismissButton]
+        
+        searchBar.frame = CGRectMake(0.0, 0.0, CGRectGetWidth(view.frame), 50.0)
+        searchBar.showsCancelButton = true
+        searchBar.inputAccessoryView = toolBar
+        searchBar.delegate = self
+        view.addSubview(searchBar)
+        
+        let demoSwitch = UISwitch(frame: CGRectMake(20.0, 60.0, 100.0, 50.0))
+        view.addSubview(demoSwitch)
+        
+        let demoButton: UIButton = UIButton(frame: CGRectMake(20.0, 100.0, 100.0, 50.0))
+        demoButton.setTitle("UIButton", forState: UIControlState.Normal)
+        demoButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+        view.addSubview(demoButton)
+        
+        let aiView: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        aiView.frame = CGRectMake(120.0, 100.0, 50.0, 50.0)
+        view.addSubview(aiView)
+        aiView.startAnimating()
+        
+        let segmentedControl: UISegmentedControl = UISegmentedControl(items: ["One", "Two"])
+        segmentedControl.frame = CGRectMake(20.0, 160.0, 200.0, 40.0)
+        segmentedControl.selectedSegmentIndex = 0
+        view.addSubview(segmentedControl)
+        
+        let slider: UISlider = UISlider(frame: CGRectMake(20.0, 210.0, 200.0, 40.0))
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = 50
+        view.addSubview(slider)
+        
+        let pageControl: UIPageControl = UIPageControl(frame: CGRectMake(0, CGRectGetHeight(view.frame) - 150, CGRectGetWidth(self.view.frame), 50) )
+        pageControl.numberOfPages = 3
+        view.addSubview(pageControl)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func dismiss()
+    {
+        searchBar.resignFirstResponder()
     }
-
 
 }
 
